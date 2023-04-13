7a1a7ae16cf9beb133881bf5413551cf3e4e8f81
EJLee1209 <dldmswo1209@gmail.com>
2023-04-13 17:12:43 +0900

    hci 플러터 프로젝트 생성

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "flutter project",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("flutter project"),
+        ),
+        body: Text("안녕하세요"),
+      ),
+    );
+  }
+}
