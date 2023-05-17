10bfef6cedfc60dcf735abf327b83fd24b9c6ce5
EJLee1209 <dldmswo1209@gmail.com>
2023-05-17 15:51:08 +0900

    UI - 매장 검색 완료

--- lib/FindScreen.dart
+++ lib/FindScreen.dart
@@ -1,4 +1,5 @@
 import 'package:app/MapScreen.dart';
+import 'package:app/SearchScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,6 +17,8 @@ class FindScreen extends StatefulWidget {
 class _FindScreenState extends State<FindScreen> {
   List<String> tabTitles = ['전체', '학원/교육', '음식점/카페', '의료/보건', '농산물/청과', '수산물/횟집', '축산물/정육', '미용/패션', '식료품/반찬'];
   List<Place> places = Place.getDummyData();
+  List<String> location = ["철원군", "화천군", "양구군", "고성군", "인제군", "춘천시", "속초시", "양양군", "홍천군", "횡성군", "원주시", "평창군", "정선군", "동해시", "영월군", "태백시", "삼척시"];
+  String _selectedLocation = "춘천시";
   int _selected = 0;
 
   @override
@@ -59,40 +62,100 @@ class _FindScreenState extends State<FindScreen> {
           padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
           height: 40,
           child: Row(
-            children: const [
-              Icon(
-                  Icons.location_on,
-                size: 20,
+            children: [
+              CupertinoButton(
+                padding: EdgeInsets.all(0),
+                  minSize: 0.0,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.location_on,
+                        size: 20,
+                      ),
+                      Text(
+                        "강원도 ${_selectedLocation}",
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13
+                        ),
+                      ),
+                      VerticalDivider(thickness: 1),
+                      const Text(
+                        "변경>",
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: (){
+                    showModalBottomSheet<void>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SizedBox(
+                          height: double.infinity,
+                          child: Expanded(
+                            child: ListView.builder(itemBuilder: (context, index) {
+                              return CupertinoButton(
+                                  child: Text(
+                                    location[index],
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                    ),
+                                  ),
+                                  onPressed: (){
+                                    // 장소 변경
+                                    setState(() {
+                                      _selectedLocation = location[index];
+                                    });
+                                    Navigator.pop(context);
+                                  }
+                              );
+                            },
+                              itemCount: location.length,
+                              padding: EdgeInsets.all(10),
+                            ),
+                          )
+                        );
+                      },
+                    );
+                  }
               ),
-              Text(
-                  "강원도 춘천시",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 13
-                ),
-              ),
-              VerticalDivider(thickness: 1),
-              Text(
-                "변경>",
-                style: TextStyle(
-                    fontSize: 13,
-                  color: Colors.grey
-                ),
-              ),
+
               Spacer(),
-              Icon(
-                  Icons.search,
-                size: 20,
-              ),
-              Text(
-                  "매장검색",
-                style: TextStyle(
-                    fontSize: 13
-                ),
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                  minSize: 0.0,
+                  child: Row(
+                    children: const [
+                      Icon(
+                          Icons.search,
+                          size: 20,
+                          color: Colors.black
+                      ),
+                      Text(
+                          "매장검색",
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.black
+                          ),
+                      )
+                    ],
+                  ),
+                  onPressed: (){
+                    // 매장 검색 화면 이동
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SearchScreen())
+                    );
+                  }
               )
             ],
           ),
         ),
+        
         // tab view
         Container(
           height: 120,
@@ -162,7 +225,6 @@ class _FindScreenState extends State<FindScreen> {
             )
         ),
         // 가맹점 리스트
-
         Expanded(
           child: ListView.builder(itemBuilder: (context, index) {
             return Padding(
---
+++ lib/SearchScreen.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'model/Place.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  String _searchKeyword = "";
+  List<String> _recentSearch = [];
+  List<Place> places = Place.getDummyData();
+  List<Place> _placesFromKeyword = [];
+  var txt = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            // AppBar
+            Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.fromLTRB(0, 70, 20, 20),
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: (){
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back_ios_new)
+                  ),
+                  Text(
+                    "매장 검색",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // 검색창
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: txt,
+                      onChanged: (newValue) {
+                        // 입력 값 변경
+                        setState(() {
+                          _searchKeyword = newValue;
+                          _placesFromKeyword.clear();
+                          places.forEach((element) {
+                            if(element.title!.contains(newValue) || element.subTitle!.contains(newValue)) {
+                              _placesFromKeyword.add(element);
+                            }
+                          });
+                        });
+                      },
+                    ),
+                  ),
+                  CupertinoButton(
+                    padding: EdgeInsets.all(0),
+                      minSize: 0.0,
+                      child: Icon(
+                        Icons.search,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                        // 검색
+                        setState(() {
+                          _recentSearch.insert(0, _searchKeyword); // 검색기록 추가
+                        });
+                      },
+                  )
+                ],
+              ),
+            ),
+
+            // 최근 검색어 기록
+            Visibility(
+                visible: _searchKeyword.isEmpty,
+                child: Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "최근 검색어",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            CupertinoButton(
+                                padding: EdgeInsets.all(0),
+                                minSize: 0.0,
+                                child: const Text(
+                                  "전체삭제",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                onPressed: (){
+                                  // 최근 검색어 전체 삭제
+                                  setState(() {
+                                    _recentSearch.clear();
+                                  });
+                                }
+                            )
+                          ],
+                        ),
+
+                        Expanded(
+                            child: ListView.builder(
+                              itemBuilder: (context, index) {
+                                return CupertinoButton(
+                                  padding: EdgeInsets.all(0),
+                                    minSize: 0.0,
+                                    onPressed: () {
+                                      // 최근 검색어 클릭 -> 입력
+                                      setState(() {
+                                        _searchKeyword = _recentSearch[index];
+                                        txt.text = _recentSearch[index];
+                                      });
+                                    },
+                                    child: Column(
+                                      children: [
+                                        SizedBox(
+                                          child: Row(
+                                            children: [
+                                              const Icon(
+                                                Icons.search,
+                                                size: 30,
+                                              ),
+                                              Expanded(
+                                                child: Text(
+                                                  _recentSearch[index],
+                                                  style: const TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 16,
+                                                  ),
+                                                ),
+                                              ),
+                                              CupertinoButton(
+                                                  padding: EdgeInsets.all(5),
+                                                  minSize: 0.0,
+                                                  child: const Icon(
+                                                    Icons.delete,
+                                                    size: 30,
+                                                    color: Colors.grey,
+                                                  ),
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      // 단일 검색 기록 삭제
+                                                      _recentSearch.removeAt(index);
+                                                    });
+                                                  }
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        const Divider(
+                                          thickness: 1.5,
+                                        )
+                                      ],
+                                    )
+                                );
+                              },
+                              itemCount: _recentSearch.length,
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+            ),
+
+            // 매장 검색 결과 리스트
+            Visibility(
+                visible: _searchKeyword.isNotEmpty,
+                child: Expanded(
+                    child: Column(
+                      children: [
+                        Container(
+                            alignment: Alignment.centerLeft,
+                            padding: EdgeInsets.all(20),
+                            child: Row(
+                              children: const [
+                                Text(
+                                  "거리순",
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Icon(Icons.keyboard_arrow_down)
+                              ],
+                            )
+                        ),
+                        Expanded(
+                          child: ListView.builder(itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: CupertinoButton(
+                                minSize: 0.0,
+                                padding: EdgeInsets.all(0),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            _placesFromKeyword.isEmpty ? "${places[index].title}" : "${_placesFromKeyword[index].title}",
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 18,
+                                                color: Colors.black
+                                            ),
+                                          ),
+                                          Text(
+                                            _placesFromKeyword.isEmpty ? "${places[index].title}" : "${_placesFromKeyword[index].subTitle}",
+                                            style: const TextStyle(
+                                                fontSize: 13,
+                                                color: Colors.grey
+                                            ),
+                                          ),
+                                          Text(
+                                            _placesFromKeyword.isEmpty ? "${places[index].description}" : "${_placesFromKeyword[index].description}",
+                                            style: const TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.black
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+
+                                    Padding(
+                                      padding: const EdgeInsets.all(12.0),
+                                      child: Column(
+                                        children: [
+                                          const Icon(
+                                            Icons.directions,
+                                            size: 50,
+                                          ),
+                                          Text(_placesFromKeyword.isEmpty ? "${places[index].distance}" : "${_placesFromKeyword[index].distance}")
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                onPressed: (){
+                                  // 가맹점 클릭 이벤트
+
+                                },
+                              ),
+                            );
+                          },
+                            itemCount: _placesFromKeyword.isEmpty ? places.length : _placesFromKeyword.length,
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                          ),
+                        ),
+                      ],
+                    )
+                ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
