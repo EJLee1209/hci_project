d011a70579bff8f5b435889320ba0e59c1c800d4
EJLee1209 <dldmswo1209@gmail.com>
2023-05-17 16:46:19 +0900

    UI - BottomAppBar 커스텀

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'HomeScreen.dart';
 import 'MapScreen.dart';
 import 'MoreScreen.dart';
 import 'MyScreen.dart';
+import 'Util/SalmonBottomBar.dart';
 
 
 void main() {
@@ -40,46 +41,95 @@ class MainView extends State<MyApp> {
         body: Center(
           child: _pages[_selectedIndex]
         ),
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          notchMargin: 8.0,
-          child: Container(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Icons.home,
-                    size: 35,
-                  ),
-                  onPressed: () => _onItemTapped(0),
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.location_on_outlined,
-                    size: 35,
-                  ),
-                  onPressed: () => _onItemTapped(1),
-                ),
-                IconButton(
-                  icon: Icon(
-                      Icons.person,
-                    size: 35,
-                  ),
-                  onPressed: () => _onItemTapped(2),
-                ),
-                IconButton(
-                  icon: Icon(
-                      Icons.more_horiz,
-                    size: 35,
-                  ),
-                  onPressed: () => _onItemTapped(3),
-                ),
-              ],
+        bottomNavigationBar: SalomonBottomBar(
+          currentIndex: _selectedIndex,
+          onTap: (i) => setState(() => _selectedIndex = i),
+          items: [
+            /// Home
+            SalomonBottomBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+                size: 30,
+              ),
+              title: Text("홈"),
+              selectedColor: Color(0xFF171559),
             ),
-          ),
-        )
+
+            /// Likes
+            SalomonBottomBarItem(
+              icon: Icon(
+                Icons.location_on_outlined,
+                size: 30,
+              ),
+              title: Text("가맹점"),
+              selectedColor: Color(0xFF171559),
+            ),
+
+            /// Search
+            SalomonBottomBarItem(
+              icon: Icon(
+                Icons.person_outline_sharp,
+                size: 30,
+              ),
+              title: Text("내 정보"),
+              selectedColor: Color(0xFF171559),
+            ),
+
+            /// Profile
+            SalomonBottomBarItem(
+              icon: Icon(
+                Icons.more_horiz_outlined,
+                size: 30,
+              ),
+              title: Text("더보기"),
+              selectedColor: Color(0xFF171559),
+            ),
+          ],
+        ),
+        // bottomNavigationBar: BottomAppBar(
+        //   shape: CircularNotchedRectangle(),
+        //   notchMargin: 8.0,
+        //   child: Container(
+        //     padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+        //     child: Row(
+        //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //       children: <Widget>[
+        //         IconButton(
+        //           icon: Icon(
+        //             _selectedIndex == 0 ? Icons.home : Icons.home_outlined,
+        //             color: _selectedIndex == 0 ? Color(0xFF171559) : Colors.grey,
+        //             size: 35,
+        //           ),
+        //           onPressed: () => _onItemTapped(0),
+        //         ),
+        //         IconButton(
+        //           icon: Icon(
+        //             _selectedIndex == 1 ? Icons.location_on : Icons.location_on_outlined,
+        //             color: _selectedIndex == 1 ? Color(0xFF171559) : Colors.grey,
+        //             size: 35,
+        //           ),
+        //           onPressed: () => _onItemTapped(1),
+        //         ),
+        //         IconButton(
+        //           icon: Icon(
+        //             _selectedIndex == 2 ? Icons.person : Icons.person_outline_sharp,
+        //             color: _selectedIndex == 2 ? Color(0xFF171559) : Colors.grey,
+        //             size: 35,
+        //           ),
+        //           onPressed: () => _onItemTapped(2),
+        //         ),
+        //         IconButton(
+        //           icon: Icon(
+        //             _selectedIndex == 3 ? Icons.more_horiz : Icons.more_horiz_outlined,
+        //             color: _selectedIndex == 3 ? Color(0xFF171559) : Colors.grey,
+        //             size: 35,
+        //           ),
+        //           onPressed: () => _onItemTapped(3),
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // )
       ),
     );
   }
