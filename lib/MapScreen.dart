d8c1eef7099f21a0e227332727c10dde57365913
EJLee1209 <dldmswo1209@gmail.com>
2023-05-18 17:03:27 +0900

    UI - Naver Map Api 연동

--- lib/MapScreen.dart
+++ lib/MapScreen.dart
@@ -1,18 +1,59 @@
 // 가맹점 찾기 - Map Screen
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:flutter/material.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
 
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  Completer<NaverMapController> _controller = Completer();
+  MapType _mapType = MapType.Basic;
+
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: NaverMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.5665, 126.9780),
-          zoom: 14.0,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            "지도 보기",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+            ),
+          ),
+          leading: IconButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                  Icons.arrow_back_ios_new,
+                color: Colors.black,
+              )
+          ),
+        ),
+        body: Container(
+          child: NaverMap(
+            onMapCreated: onMapCreated,
+            mapType: _mapType,
+          ),
         ),
       ),
     );
+  }
+
+  void onMapCreated(NaverMapController controller) {
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
   }
 }
